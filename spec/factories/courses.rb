ab82b660be1e275743d727c99940af677dcfbc63
gxbsst <gxbsst@gmail.com>
2014-10-29 17:26:33 +0800

    2.4

---
+++ spec/factories/courses.rb
@@ -0,0 +1,20 @@
+FactoryGirl.define do
+  sequence :name do |n|
+    "course_name_#{n}"
+  end
+  factory :course do
+    name
+    intro "here is intro"
+    description "here is description"
+    cover_url "#"
+    released_at Time.now
+
+    after(:create) do |course|
+      create_list :character, 3, course: course
+    end
+
+    trait :type_is_nil do
+      type ''
+    end
+  end
+end
